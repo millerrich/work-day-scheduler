8c0c0f756c2a738fae338dc776f846955077c8d9
Miller Rich <william.miller.rich@gmail.com>
2020-08-28 16:45:17 -0500

    add functions renderEvents, storeEvents, init in script.js

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 
-$('.saveBtn').on("click", function() {
-    console.log("click");
-});
+var schedule = [];
+var eventInput;
+
+function init() {
+    var storedEvents = JSON.parse(localStorage.getItem("schedule"));
+        if(storedEvents) {
+            schedule = storedEvents;
+        }
+        renderEvents();
+}
+
+function renderEvents() {
+    for (var i = 0; i < schedule.length; i++) {
+        var index = schedule[i];
+        var time = index.hour;
+
+        $("[data-hour=" + time + "]").val(index.eventTitle);
+    }
+}
+    
+function storeEvents() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+$('.saveBtn').on("click", function (event) {
+    event.preventDefault();
+    var $input = $(this).prev();
+
+        var userEvent = {
+            eventTitle: $input.val(),
+            hour: $input.attr("data-hour")
+        };
+        console.log($input.val());
+
+        schedule.push(userEvent);
+        storeEvents();
+        renderEvents();
+})
+init();
